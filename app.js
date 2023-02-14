c6ff28875c7be4c0bc15631cd02329122732877a
Rimma27 <rimmaady@yandex.ru>
2023-02-14 21:15:21 +0300

    change port

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const auth = require('./middlewares/auth');
 const { validationCreateUser, validationLogin } = require('./middlewares/validation');
 const centralizedHandler = require('./middlewares/centralizedHandler');
 
-const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3001, MONGO_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const app = express();
 
 app.use(express.json());
